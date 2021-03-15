94349f81a2309f6283c39945227c91da5bd99af2
leehuhlee <leehuhlee@gmail.com>
2021-03-16 07:31:18 +0900

    MMORPG Data

--- MMO_RPG/Assets/Scripts/Managers/Managers.cs
+++ MMO_RPG/Assets/Scripts/Managers/Managers.cs
@@ -7,6 +7,7 @@ public class Managers : MonoBehaviour
     static Managers s_instance;
     static Managers Instance{ get { Init(); return s_instance; } }
 
+    DataManager _data = new DataManager();
     InputManager _input = new InputManager();
     PoolManager _pool = new PoolManager();
     ResourceManager _resource = new ResourceManager();
@@ -14,6 +15,7 @@ public class Managers : MonoBehaviour
     SoundManager _sound = new SoundManager();
     UIManager _ui = new UIManager();
 
+    public static DataManager Data { get { return Instance._data; } }
     public static InputManager Input { get { return Instance._input; } }
     public static PoolManager Pool { get { return Instance._pool; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
@@ -43,6 +45,8 @@ public class Managers : MonoBehaviour
             }
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<Managers>();
+
+            s_instance._data.Init();
             s_instance._pool.Init();
             s_instance._sound.Init();
         }
--- MMO_RPG/Assets/Scripts/Scenes/GameScene.cs
+++ MMO_RPG/Assets/Scripts/Scenes/GameScene.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
-
     protected override void Init()
     {
         base.Init();
@@ -13,30 +11,7 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
-        StartCoroutine("CoStopExplode", 6.0f);
-    }
-
-    IEnumerator CoStopExplode(float seconds)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Stop Execute!");
-
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator ExplodeAfterSeconds(float seconds)
-    {
-        Debug.Log("Explode Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explode Execute!");
-        co = null;
+        Dictionary<int,Stat> dict = Managers.Data.StatDict;
     }
 
     public override void Clear()
