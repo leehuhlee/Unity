75a4c40ff90644f7191bf159216a72a0d6031420
leehuhlee <leehuhlee@gmail.com>
2021-08-02 21:08:15 +0200

    Unity MMO Game

---
+++ MMO_Game/Common/protoc-3.12.3-win64/bin/GenProto.bat
@@ -0,0 +1,10 @@
+protoc.exe -I=./ --csharp_out=./ ./Protocol.proto 
+IF ERRORLEVEL 1 PAUSE
+
+START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
+XCOPY /Y Protocol.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y Protocol.cs "../../../Server/DummyClient/Packet"
+COPY /Y Protocol.cs "../../../Server/Server/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../Server/DummyClient/Packet"
+XCOPY /Y ServerPacketManager.cs "../../../Server/Server/Packet"
